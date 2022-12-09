64d1147fe87da63d421a92dd86012614221eb781
Victor-Iulian Sandu <vyctor661@gmail.com>
2022-12-09 23:38:03 +0200

    forms validation

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -4,17 +4,58 @@ import Icon from "@mdi/react";
 import "./Login.css";
 
 import Navbar from "../../common/components/navbar/Navbar";
+import { useForm } from "react-hook-form";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+type FormData = {
+    username: string;
+    password: string;
+};
+
+const schema = yup
+    .object({
+        username: yup.string().required("No username provided."),
+        password: yup.string().required("No password provided."),
+    })
+    .required();
 
 const Login = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormData>({
+        resolver: yupResolver(schema),
+    });
+    const onSubmit = (data: FormData) => console.log(data);
+
     return (
         <div className="homepageContainer">
             <Navbar></Navbar>
 
             <div className="pageContainer">
-                <form className="loginRegisterForm">
+                <form
+                    className="loginRegisterForm"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
                     <div className="loginRegisterFormTitle">LOGIN</div>
-                    <input type="text" placeholder="Username" />
-                    <input type="password" placeholder="Password" />
+                    <input
+                        type="text"
+                        {...register("username")}
+                        placeholder="Username"
+                    />
+                    <p className="fieldError">{errors.username?.message}</p>
+                    <input
+                        type="password"
+                        {...register("password")}
+                        placeholder="Password"
+                    />
+                    <p className="fieldError">{errors.password?.message}</p>
+                    <a href="/register" className="forgotPassword">
+                        Forgot password?
+                    </a>
                     <input type="submit"></input>
                     <a href="/register" className="registerInstead">
                         Not a member? Signup now
--- src/app/pages/register/Register.tsx
+++ src/app/pages/register/Register.tsx
@@ -4,19 +4,83 @@ import Icon from "@mdi/react";
 import "./Register.css";
 
 import Navbar from "../../common/components/navbar/Navbar";
+import { useForm } from "react-hook-form";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+type FormData = {
+    username: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+};
+
+const schema = yup
+    .object({
+        username: yup.string().required("No username provided."),
+        email: yup
+            .string()
+            .email("Not a valid email address.")
+            .required("No email provided."),
+        password: yup
+            .string()
+            .min(8, "Password is too short - should be 8 characters minimum.")
+            .matches(
+                /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+                "Password must contain at least one letter, one number and one special character."
+            ),
+        confirmPassword: yup
+            .string()
+            .required("No password provided")
+            .oneOf([yup.ref("password"), null], "Passwords must match"),
+    })
+    .required();
 
 const Register = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormData>({ resolver: yupResolver(schema) });
+    const onSubmit = (data: FormData) => console.log(data);
+
     return (
         <div className="homepageContainer">
             <Navbar></Navbar>
 
             <div className="pageContainer">
-                <form className="loginRegisterForm">
+                <form
+                    className="loginRegisterForm"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
                     <div className="loginRegisterFormTitle">CREATE ACCOUNT</div>
-                    <input type="text" placeholder="Username" />
-                    <input type="email" placeholder="Email" />
-                    <input type="password" placeholder="Password" />
-                    <input type="password" placeholder="Confirm Password" />
+                    <input
+                        type="text"
+                        {...register("username")}
+                        placeholder="Username"
+                    />
+                    <p className="fieldError">{errors.username?.message}</p>
+                    <input
+                        type="email"
+                        {...register("email")}
+                        placeholder="Email"
+                    />
+                    <p className="fieldError">{errors.email?.message}</p>
+                    <input
+                        type="password"
+                        {...register("password")}
+                        placeholder="Password"
+                    />
+                    <p className="fieldError">{errors.password?.message}</p>
+                    <input
+                        type="password"
+                        {...register("confirmPassword")}
+                        placeholder="Confirm Password"
+                    />
+                    <p className="fieldError">
+                        {errors.confirmPassword?.message}
+                    </p>
                     <input type="submit"></input>
                     <a href="/login" className="registerInstead">
                         Already a member? Sign in
